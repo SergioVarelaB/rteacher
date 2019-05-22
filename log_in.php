db9c4df614449281717c935c2d31ec796227dad1
SergioVarelaB <sergioalberto.varelab@gmail.com>
2019-05-21 20:02:58 -0600

    cambie el select

--- log_in.php
+++ log_in.php
@@ -4,33 +4,16 @@ session_start();
 
 <!DOCTYPE html>
 <html lang="es">
-<link href="user\css\style.css" rel="stylesheet" type="text/css" />
+<!--<link href="user\css\style.css" rel="stylesheet" type="text/css" />-->
 
 <style>
-input{
-        border: 20px;
-        color: #4d4dff ; 
-}
-input:focus {
-  outline: 10px;
-  border-color: #e6e6ff;
-}
-input:focus + .input-icon i {
-  color: #f5a3df;
-}
-input:focus + .input-icon:after {
-  border-right-color: #e6e6ff;
-}
-input[type="radio"] {
-  display: none;
-}
-
 h3 , h2.tituloChido{
     font-family: arial;
     color:  #000000;
 
 }
     body,head{
+        font-family: 'Open Sans', 'Helvetica Neue', Helvetica, Arial, sans-serif;
         background-image: url("user/imagenes/fondo3.png") ;
         background-repeat: repeat;
         position: relative;
@@ -44,13 +27,28 @@ h3 , h2.tituloChido{
         -webkit-filter: blur(5px); /* Safari 6.0 - 9.0 */
         filter: blur(5px);
     }
-    input {
-        width: 400px;
-        position: right;
-    }
 
-
-    
+  .form {
+  position: relative;
+  z-index: 1;
+  background: #d0d0d0;
+  max-width: 360px;
+  margin: 100px auto 100px;
+  padding: 45px;
+  text-align: center;
+  box-shadow: 0 0 20px 0 rgba(0, 0, 0, 0.2), 0 5px 5px 0 rgba(0, 0, 0, 0.24);
+}
+.form input {
+  font-family: "Roboto", sans-serif;
+  outline: 0;
+  background: #1179c6;
+  width: 100%;
+  border: 0;
+  margin: 0 0 15px;
+  padding: 15px;
+  box-sizing: border-box;
+  font-size: 14px;
+}   
 </style>
 
 <head>
@@ -63,13 +61,14 @@ h3 , h2.tituloChido{
 </head>
 <body>
     
-    <form action="" method="post">
+    <form action="" method="post" class = "form">
         <p><h2 class = "tituloChido">Username:</h2> <input type="text" name="username" id="" required></p>
         <p><h2  class = "tituloChido" >Contraseña: </h2><input type="password" name="password" id="" required></p>
         <input type="submit" value="Entrar" name = "entrar">
+        <h3>No tienes una cuenta?</h3>
+        <a class = "tituloChido" href="user/sign_up.php">Registrate aqui</a>
     </form>
-    <h3>No tienes una cuenta?</h3>
-    <a href="user/sign_up.php">Registrate aqui</a>
+    
 </body>
 </html>
 
@@ -82,7 +81,7 @@ h3 , h2.tituloChido{
         $conn = getConexionUsuario();
         if(mysqli_connect_errno($conn))
         {
-            echo 'No se pudo hacer la conexión con la base de datos';
+            echo 'No mancges, algo paso';
             exit;
         }
     
